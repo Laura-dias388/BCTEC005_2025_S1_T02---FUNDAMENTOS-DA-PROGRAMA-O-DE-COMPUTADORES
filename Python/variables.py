7a6ab91d0bfcdbe79ccdc22b8e96edb00d74358d
Laura <ldiasvitaltoledo8@gmail.com>
2025-03-18 00:41:26 -0300

    My first commit

---
+++ Python/variables.py
@@ -0,0 +1,28 @@
+numero = 20
+nome = 'Maria'
+decimal = 8.9
+verdade = False
+complexo= 3j 
+
+
+# variavel do tipo inteiro 
+print(type(numero))
+
+# variavel do tipo string 
+print(type(nome))
+
+# variavel do tipo float 
+print(type(decimal))
+
+# variavel do tipo bool 
+print(type(verdade))
+
+# variavel do tipo complex 
+print(type(complexo))
+
+teste = verdade
+
+# variavel do tipo complex 
+print(type(teste))
+
+print(isinstance(teste,str))
---
+++ README.md
@@ -0,0 +1,11 @@
+# Nesta pasta farei meus trabalhos para o curso de Introdução à Redes de Computadores!
+
+## UNIFEI 
+
+# Professsor: FISCHER JONATAS FERREIRA 
+<details>
+  <summary><strong>🤷🏽‍♀️ O que sera feito:</strong></summary><br />
+
+  Irei aplicar códigos para a resolução dos exercícios propostos na aula.
+
+</details>
