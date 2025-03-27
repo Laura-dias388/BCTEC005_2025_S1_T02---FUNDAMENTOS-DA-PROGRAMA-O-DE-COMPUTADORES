d7708990064c26a54c6a0955b473a8a013aa23cd
Laura <ldiasvitaltoledo8@gmail.com>
2025-03-27 02:40:26 -0300

    Exercício complementar, viagem para Marte

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # BCTEC005_2025_S1_T02---FUNDAMENTOS-DA-PROGRAMA-O-DE-COMPUTADORES
-Exercícios do BCTec  FUNDAMENTOS DA PROGRAMAÇÃO DE COMPUTADORES
+
+# Exercício adicional sobre a tutoria: Bagagem Espacial – Quanto Você Pode Levar?Tarefa
+
+<details>
+  <summary><strong>🤷🏽‍♀️ Você tem uma missão:</strong></summary><br />
+
+  Durante a missão para Marte, cada tripulante poderá levar uma bagagem pessoal. No entanto, por questões de peso e segurança, o limite é rigoroso. Sua missão agora é calcular a quantidade total de bagagem permitida e o espaço utilizado pelos tripulantes com base em suas escolhas.
+
+</details>
+
+<details>
+  <summary><strong>🤷🏽‍♀️ O que sera feito:</strong></summary><br />
+
+  Acrecente ao programa feito na Tutoria 2 (25/03) as seguintes novas funcioanlidades:
+
+1) O volume total disponível de bagagem para todos os tripulantes juntos
+
+2) O volume utilizado por todos os tripulantes, com base nas escolhas individuais
+
+3) O espaço restante de bagagem disponível na nave
+
+</details>
+
+<details>
+  <summary><strong>Detalhes</strong></summary><br />
+
+  Interação do seu programa
+
+  Seu programa deverá solicitar ao usuário:
+
+  Quantidade de tripulantes
+
+  Limite de volume de bagagem por tripulante (em litros)
+
+  Volume da mochila escolhida por cada tripulante (em média, em litros)
+
+  Volume da mala escolhida por cada tripulante (em média, em litros)
+
+  Regras para o cálculo
+  Cada tripulante pode levar uma mochila e uma mala
+
+  Volume total disponível: quantidade_de_tripulantes * limite_por_tripulante
+
+  Volume utilizado: quantidade_de_tripulantes * (volume_mochila + volume_mala)
+
+  Espaço restante: volume_total_disponivel - volume_utilizado
+
+</details>
+
---
+++ exercicio_nasa/exercicio.py
@@ -0,0 +1,41 @@
+print(f"========================================================================================")
+
+limite_por_tripulante = 500
+
+nome_comandante=input("Nome do astronauta comandante da missão: ")
+distancia= float(input("Distancia da Terra ate Marte (em milhoes de km): ")) # a média é de 225 milhões de quilômetros.
+quantidade_de_tripulantes= int(input("Quantidade de tripulantes na nave: "))
+consumo_combustivel=float(input("Consumo de combustível por milhão de km (em litros): "))
+custo=float(input("Custo do litro de combustível (em reais): "))
+refeicao= int(input("Quantidade média de refeições por dia por tripulante: "))
+duracao=float(input("Duração estimada da viagem (em dias): "))
+volume_mochila = float(input("Volume da sua mochila (em litros): "))
+volume_mala = float(input("Volume da mala (em litros): "))
+
+print(f"========================================================================================")
+
+total_combustivel = distancia * consumo_combustivel
+
+custo_total_combustivel = total_combustivel * custo
+
+total_refeicoes = quantidade_de_tripulantes *  refeicao * duracao
+
+volume_total_disponivel = quantidade_de_tripulantes * limite_por_tripulante
+
+volume_utilizado = quantidade_de_tripulantes * (volume_mochila + volume_mala)
+  
+espaço_restante = volume_total_disponivel - volume_utilizado
+
+
+
+print(f"Nome do astronauta comandante da missão: {nome_comandante}")
+print(f"Distancia da Terra ate Marte (em milhoes de km): {distancia}")
+print(f"Total de combustivel: {total_combustivel}")
+print(f"Custo total do combustivel: {custo_total_combustivel}")
+print(f"========================================================================================")
+print(f"Total refeicoes: {total_refeicoes}")
+print(f"O total de tripulantes é: {quantidade_de_tripulantes}")
+print(f"O volume total disponível é: {volume_total_disponivel}")
+print(f"O volume utilizado foi de: {volume_utilizado}")
+print(f"========================================================================================")
+print(f"O espaço restante é de: {espaço_restante}")
