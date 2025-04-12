411fcd85e72a8ebda80ef3ec5f51c50e14d801a5
Laura <ldiasvitaltoledo8@gmail.com>
2025-04-12 11:52:33 -0300

    feat: Adicionando resolulção dos exercícios em grupo

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,48 @@
 # 1: Restrições dos dados de entrada
 
-number = int(input(f"Qual o valor de qualificação: "))
+number = int(input(f"Qual o valor de qualificação do seu personagem: "))
 
-if (number % 4 == 0) and (number <100):
+if (number % 4 == 0) and (number < 100):
   print(f"Alex você esta qualificado")
+else:
+  print(f"Personagem não quallificado")  
+
+print(f"Seu nível de energia é: ")
+
+print(f"=============================================================================")
+
+energy = int(number * 0.6)
+
+if energy >= 30:
+  print(f"Alex seu nível de energia é : {energy} e está dentro do esperado!")
+else:
+  print(f"Seu nível de energia esta baixo, tente novamente!!")  
+
+print(f"=============================================================================")
+
+key_master = str(input(f"Você está em posse da Chave Mestra (Sim/Não): "))
+
+key_master_true = key_master == "Sim"
+
+type_key = type(key_master_true)
+print(type_key)
+
+if key_master_true == "Sim":
+  print(f"Uall Você pode seguir seu caminho pelos templo dos códigos fragmentados!!")
+else:
+  print(f"Lamento sem a chave mestra você não tera chance!")  
+
+
+# number = int(input(f"Qual o valor de qualificação do seu personagem: "))
+# if (number % 4 == 0) and (number <100):
+#   print(f"Alex você esta qualificado")
+
+# number = int(input(f"Qual o valor de qualificação do seu personagem: "))
+
+# if (number % 4 == 0) and (number <100):
+#   print(f"Alex você esta qualificado")
+
+# number = int(input(f"Qual o valor de qualificação do seu personagem: "))
+
+# if (number % 4 == 0) and (number <100):
+#   print(f"Alex você esta qualificado")
