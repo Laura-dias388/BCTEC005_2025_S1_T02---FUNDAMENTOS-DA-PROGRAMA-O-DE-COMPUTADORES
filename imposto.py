0b552cb07b8dba33c53d6ae99efedc35959a83c7
Laura <ldiasvitaltoledo8@gmail.com>
2025-04-25 11:57:30 -0300

    style: Adicionando comentários e melhorando os espaçamentos

--- imposto.py
+++ imposto.py
@@ -3,18 +3,21 @@ print("=========================================================================
 login = input("Informe seu login: ")
 senha = input("Informe sua senha: ")
 
-
+imposto_075 = 33919.80 - 22847.76
+imposto_15 = 45012.60 - 33919.80
+          
 if (login == "ana" and senha == "12345") or (login == "joao" and senha == "54321"):
     print("Você está logado. Seja bem vindo!")
 
     renda_anual = float(input("Digite sua renda anual (em R$): "))
-    tipo_imposto = int(input("Escolha o tipo de imposto >>1<< para simplificado e >>2<< para completo: "))
+    tipo_imposto = int(input("Escolha o tipo de imposto >> 1 << para simplificado e >> 2 << para completo: "))
     despesas_medicas = float(input("Digite suas despesas médicas (em R$): "))
     despesas_escolares = float(input("Digite suas despesas escolares (em R$): "))
     despesas_livros = float(input("Digite suas despesas com livros (em R$): "))
     despesas_viagens = float(input("Digite suas despesas com viagens (em R$): "))
     DESPESAS = ((despesas_medicas * 0.05) + (despesas_escolares * 0.10) + (despesas_livros * 0.02) + (despesas_viagens * 0.30))
 
+    # válido para imposotos abaixo de 45012.60
     if tipo_imposto == 1 and renda_anual <= 45012.60:
         imposto = 0
         imposto_real = renda_anual * 0.09
@@ -23,29 +26,26 @@ if (login == "ana" and senha == "12345") or (login == "joao" and senha == "54321
     else:    
 
     # imposto=0
-      if renda_anual <= 22847.76:
+      if renda_anual <= 22847.76:  #abaixo de 22847.76
           imposto = 0
           
-      elif renda_anual <= 33919.80:
+      elif renda_anual <= 33919.80:  #abaixo de 33919.80
           imposto_22 = renda_anual - 22847.76
           imposto_real = imposto_22 * 0.075
 
-      elif renda_anual <= 45012.60:
+      elif renda_anual <= 45012.60:  #abaixo de 45012.60
           imposto_33 = renda_anual - 33919.80
-          imposto_22 = 33919.80 - 22847.76
-          imposto_real = (imposto_22 * 0.075) + (imposto_33 * 0.15)
+          imposto_real = (imposto_075 * 0.075) + (imposto_33 * 0.15)
 
-      elif renda_anual <= 55976.16:
+      elif renda_anual <= 55976.16:  #abaixo de 55976.16
           imposto_45 = renda_anual - 45012.60
-          imposto_33 = 45012.60 - 33919.80
-          imposto_22 = 33919.80 - 22847.76
-          imposto_real = (imposto_22 * 0.075) + (imposto_33 * 0.15) + (imposto_45 * 0.225)
+          imposto_real = (imposto_075 * 0.075) + (imposto_15 * 0.15) + (imposto_45 * 0.225)
+
       else:
-          imposto_55 = renda_anual - 55976.16
+          imposto_55 = renda_anual - 55976.16  #acima de 55976.16
           imposto_45 = 55976.16 - 45012.60
-          imposto_33 = 45012.60 - 33919.80
-          imposto_22 = 33919.80 - 22847.76
-          imposto_real = (imposto_22 * 0.075) + (imposto_33 * 0.15) + (imposto_45 * 0.225) + (imposto_55 * 0.275)
+          
+          imposto_real = (imposto_075 * 0.075) + (imposto_15 * 0.15) + (imposto_45 * 0.225) + (imposto_55 * 0.275)
 
       imposto = imposto_real - DESPESAS
       print(f"O valor do seu imposto após as deduções será de: { imposto:.2f}")
