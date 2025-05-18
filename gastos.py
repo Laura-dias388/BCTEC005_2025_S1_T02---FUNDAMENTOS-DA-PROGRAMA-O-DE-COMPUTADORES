dcb5ae2e7664024d7689cefff96937d1ed99f2b4
Laura <ldiasvitaltoledo8@gmail.com>
2025-05-17 23:00:56 -0300

    feat: Tentando o exercício 10

--- gastos.py
+++ gastos.py
@@ -36,7 +36,7 @@ print("Exercício 4")
 soma_gastos_ana = 0
 for indice, conteudo_linha in df.iterrows():
     if(conteudo_linha["responsável"].lower() == "ana"):
-       soma_gastos_ana = soma_gastos_ana + conteudo_linha["valor"]
+        soma_gastos_ana = soma_gastos_ana + conteudo_linha["valor"]
 print(f"Valor total de gastos da Ana foi:>> {soma_gastos_ana: .2f} ")
 
 print("==============================================================================================")
@@ -75,22 +75,20 @@ print("Exercício 7")
 maior_gasto = 0
 for indice, conteudo_linha in df.iterrows():
     if(conteudo_linha["valor"] > maior_gasto):
-      maior_gasto = conteudo_linha["valor"]
-      categoria = conteudo_linha["categoria"]
-      responsavel = conteudo_linha["responsável"]
+        maior_gasto = conteudo_linha["valor"]
+        categoria = conteudo_linha["categoria"]
+        responsavel = conteudo_linha["responsável"]
 print(f"O maior gasto registrado foi: {maior_gasto: .2f} a categoria é {categoria} e o/a responsável é: {responsavel}")
 
 print("==============================================================================================")
 print("Exercício 8")
 
-maior_numero_registros = 0
 cont1 = 0
 cont2 = 0
 cont3 = 0
 cont4 = 0
 cont5 = 0
 cont6 = 0
-maior = 0
 
 for indice, conteudo_linha in df.iterrows():
     if(conteudo_linha["categoria"].lower() == "alimentação"):
@@ -105,32 +103,19 @@ for indice, conteudo_linha in df.iterrows():
         cont5 += 1
     if(conteudo_linha["categoria"].lower() == "consultoria"):
         cont6 += 1 
-        
-if(cont1 > maior):
-    maior += 1 
-    nome =  "alimentação"
 
-if(cont2 > maior):
-    maior += 1 
-    nome =  "transporte"
-
-if(cont3 > maior):
-    maior += 1 
-    nome =  "hospedagem"
-
-if(cont4 > maior):
-    maior += 1 
-    nome =  "material de escritório"
-
-if(cont5 > maior):
-    maior += 1 
-    nome =  "cursos"
-
-if(cont6 > maior):
-    maior += 1 
-    nome =  "consultoria"
-
-print(f"A categoria com maior registros de gastos foi:{nome}")
+if(cont1 > cont2 and cont1 > cont3 and cont1 > cont4 and cont1 > cont5 and cont1 > cont6):
+    print(f"A categoria com maior registros de gastos foi: alimentação")
+if(cont2 > cont1 and cont2 > cont3 and cont2 > cont4 and cont2 > cont5 and cont2 > cont6):    
+    print(f"A categoria com maior registros de gastos foi: transporte")
+if(cont3 > cont2 and cont3 > cont1 and cont3 > cont4 and cont3 > cont5 and cont3 > cont6):
+    print(f"A categoria com maior registros de gastos foi: hospedagem")
+if(cont4 > cont2 and cont4 > cont3 and cont4 > cont1 and cont4 > cont5 and cont4 > cont6):    
+    print(f"A categoria com maior registros de gastos foi: material de escritório")
+if(cont5 > cont2 and cont5 > cont3 and cont5 > cont4 and cont5 > cont1 and cont5 > cont6):
+    print(f"A categoria com maior registros de gastos foi: cursos")
+if(cont6 > cont2 and cont6 > cont3 and cont6 > cont4 and cont6 > cont5 and cont6 > cont1):    
+    print(f"A categoria com maior registros de gastos foi: consultoria")    
 
 print("==============================================================================================")
 print("Exercício 9")
@@ -141,14 +126,37 @@ nome = input("Digite o nome de um responsável pelos gastos: ")
 for indice, conteudo_linha in df.iterrows():
     if(conteudo_linha["responsável"].lower() == nome.lower()):
         gastos_por_responsavel += conteudo_linha["valor"]
+   
 print(f"O valor gasto por {nome} foi: {gastos_por_responsavel: .2f}")
 
 print("==============================================================================================")
-# print("Exercício 10")
+print("Exercício 10")
 
-# datas = []
+
+# lista_data = []
+# resultado = []
 
 # for indice, conteudo_linha in df.iterrows():
+    
 #     if(conteudo_linha["valor"] > 500):
-#         datas += conteudo_linha["data"]
-#         print(f"As datas em que o valor de gastos ultrapassa 500,00 são: {datas}")
+#         if(conteudo_linha["data"]):
+#             lista_data.append(conteudo_linha['data'])
+
+# for data in lista_data:
+#     for i in lista_data:
+#         if(data == i):
+#             resultado.append(data)
+
+# for resp in resultado:
+#     print(f"As data em que todos os gastos foram maiores que R$ 500,00 são: {resp}")
+
+for indice, conteudo_linha in df.iterrows():
+    if(conteudo_linha["valor"] > 500):
+        
+        data = conteudo_linha["data"]
+    print(f"As datas em que os gastos ultrapassam R$ 500,00 são: {data}")    
+
+# numbers = [75, 51, 92, 24, 62, 88]
+# print(f"Lista inicial: {numbers}")
+# for i in numbers:
+#     print(i)
