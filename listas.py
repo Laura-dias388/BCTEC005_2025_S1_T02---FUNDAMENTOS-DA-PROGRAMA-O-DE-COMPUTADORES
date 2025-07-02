daa94d2badd6ac3c466844de83d6b2c9fd715638
Laura <ldiasvitaltoledo8@gmail.com>
2025-07-02 02:37:29 -0300

    Adicionando a lógica do exercicio

--- listas.py
+++ listas.py
@@ -22,9 +22,10 @@ for i in lista_cem:
   if i > 0:
     lista_positivos.append(i)
 
-print(lista_positivos)    
+print(lista_positivos)   
+
 print("----------------------------------------------------------------")
-print("b) Imprima os números negaivos")
+print("b) Imprima os números negativos")
 
 lista_negativos = []
 
@@ -32,42 +33,127 @@ for i in lista_cem:
   if i < 0:
     lista_negativos.append(i)
 
-print(lista_negativos) 
+print(lista_negativos)
+
 print("----------------------------------------------------------------")
 print("c) Imprima os números pares")
+lista_pares = []
+
+for i in lista_cem:
+  if(i % 2 == 0):
+    lista_pares.append(i)
+print(lista_pares) 
+
 print("----------------------------------------------------------------")
 print("d) Imprima os números ímpares")
+lista_impares = []
+
+for i in lista_cem:
+  if(i % 2 != 0):
+    lista_impares.append(i)
+print(lista_impares) 
+
 print("----------------------------------------------------------------")
 print("e) Imprima os números múltiplos de 4")
+lista_multiplos_4 = []
+
+for i in lista_cem:
+  if(i % 4 == 0):
+    lista_multiplos_4.append(i)
+print(lista_multiplos_4) 
+
 print("----------------------------------------------------------------")
 print("f) Imprima o maior número da lista")
+
+maior = 0
+
+for i in lista_cem:
+  if i > maior:
+    maior = i
+
+print(maior)
+
 print("----------------------------------------------------------------")
 print("g) Imprima o menor número da lista")
+
+menor = lista_cem[0] #evitar bugs acessando o primeiro elemento e fazendo a verificação
+
+for i in lista_cem:
+  if i < menor:
+    menor = i
+
+print(menor)
+
 print("----------------------------------------------------------------")
 print("h) Imprima a média dos número da lista")
+
+soma = 0
+
+for i in lista_cem:
+  soma += i
+media = soma / len(lista_cem)
+
+print(f"A soma será de {soma} e a média será {media}")
+
 print("----------------------------------------------------------------")
 print("i) Ordene os elementos da lista em ordem crescente")
 
+order = sorted(lista_cem)
+print(order)
 
 print("================================================================")
 print("Exercício - 02")
  
 print("----------------------------------------------------------------")
 print("a) Imprima o terceiro elemento da lista")
+
+print(lista_cem[2])
+
 print("----------------------------------------------------------------")
 print("b) Acesse o último elemento da lista")
+
+print(lista_cem[-1])
+
 print("----------------------------------------------------------------")
 print("c) Modifique o quinto elemento da lista para o número 50")
+
+lista_cem[4] = 50
+print(lista_cem)
+
 print("----------------------------------------------------------------")
 print("d) Adicione o número 11 ao final da lista")
+
+lista_cem[-1] = 11
+print(lista_cem)
+
 print("----------------------------------------------------------------")
 print("e) Insira o número 0 no início da lista")
+
+lista_cem[0] = 0
+print(lista_cem)
+
 print("----------------------------------------------------------------")
 print("f) Remova o último elemento da lista")
+
+lista_cem.pop()
+print(lista_cem)
+
 print("----------------------------------------------------------------")
 print("g) Remova o elemento 50 da lista")
+
+lista_cem.remove(lista_cem[49])
+print(lista_cem)
+
 print("----------------------------------------------------------------")
 print("h) Verifique se o número 5 está na lista")
+
+if 5 in lista_cem:
+  
+  print("O número 5 está na lista!!")
+
+else:
+  print("Não tem número 5 nessa lista!")  
+
 print("================================================================")
 print("Exercício - 03")
 print("Crie uma nova lista que contenha os quadrados dos números de 1 a 10")
